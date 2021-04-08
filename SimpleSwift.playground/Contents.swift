efd202e115d7ed2d41c6d50b0b18f74282bfbb4e
Hailun Zhang <annienobear@Hailuns-MacBook-Pro.local>
2021-04-09 01:25:23 +0800

    passtest

--- SimpleSwift.playground/Contents.swift
+++ SimpleSwift.playground/Contents.swift
@@ -1,12 +1,55 @@
 print("Welcome to the UW Calculator Playground")
 
 func calculate(_ args: [String]) -> Int {
-    return -1
+    switch args.last {
+    case "count":
+        return args.count - 1
+    case "avg":
+        if args.count == 1 {return 0}
+        var total = 0
+        for index in 0...(args.count - 1) {
+            total += Int(args[index]) ?? 0
+        }
+        return total/(args.count - 1)
+    case "fact":
+        if args.count == 1 {return 0}
+        let num = Int(args[0]) ?? 0
+        var count = 1
+        var total = 1
+        while count <= num {
+            total *= count
+            count += 1
+        }
+        return total
+    default:
+        let numA = Int(args[0]) ?? 0
+        let numB = Int(args[2]) ?? 0
+        switch args[1] {
+        case "+":
+            return numA + numB
+        case "-":
+            return numA - numB
+        case "*":
+            return numA * numB
+        case "/":
+            return numA / numB
+        case "%":
+            return numA % numB
+        default:
+            return -1
+        }
+    }
 }
 
+
 func calculate(_ arg: String) -> Int {
-    return -1
-}
+    let lis = arg.split(separator: " ")
+    var input: [String] = []
+    for item in lis {
+        input.append(String(item))
+    }
+    return calculate(input)
+ }
 
 // -------------------------------------------
 // All of these expressions should return true
@@ -53,7 +96,7 @@ calculate("5 fact") == 120
 
 // Implement calculate([String]) and calculate(String)
 // to handle negative numbers
-/*
+
 calculate(["2", "+", "-2"]) == 0
 calculate(["2", "-", "-2"]) == 4
 calculate(["2", "*", "-2"]) == -4
@@ -68,16 +111,59 @@ calculate("2 - -2") == 4
 calculate("-2 / 2") == -1
 
 calculate("1 -2 3 -4 5 count") == 5
-*/
+
  
 // Implement calculate([String]) and calculate(String)
 // to use floating-point values
-/*
+
+
 func calculate(_ args: [String]) -> Double {
-    return -1.0
+    switch args.last {
+    case "count":
+        return Double(args.count - 1)
+    case "avg":
+        if Double(args.count) == 1.0 {return 0.0}
+        var total = 0.0
+        for index in 0...(args.count - 1) {
+            total += Double(args[index]) ?? 0
+        }
+        return total/Double((args.count - 1))
+    case "fact":
+        if Double(args.count) == 1.0 {return 0.0}
+        let num = Double(args[0]) ?? 0
+        var count = 1.0
+        var total = 1.0
+        while count <= num {
+            total *= count
+            count += 1.0
+        }
+        return total
+    default:
+        let numA = Double(args[0]) ?? 0
+        let numB = Double(args[2]) ?? 0
+        switch args[1] {
+        case "+":
+            return numA + numB
+        case "-":
+            return numA - numB
+        case "*":
+            return numA * numB
+        case "/":
+            return numA / numB
+        case "%":
+            return numA.truncatingRemainder(dividingBy: numB)
+        default:
+            return -1
+        }
+    }
 }
 func calculate(_ arg: String) -> Double {
-    return -1.0
+    let lis = arg.split(separator: " ")
+    var input: [String] = []
+    for item in lis {
+        input.append(String(item))
+    }
+    return calculate(input)
 }
 
 calculate(["2.0", "+", "2.0"]) == 4.0
@@ -87,4 +173,3 @@ calculate(["2.5", "*", "2.5"]) == 6.25
 calculate(["2.0", "/", "2.0"]) == 1.0
 calculate(["2.0", "%", "2.0"]) == 0.0
 calculate("1.0 2.0 3.0 4.0 5.0 count") == 5
-*/
